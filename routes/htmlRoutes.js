4e92eaee983099166cc62ede693114dabedee1cf
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-08-28 15:18:58 +0800

    Initial commit

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..public/index.html"));
+});
