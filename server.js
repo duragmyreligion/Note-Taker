aa9bd5116f9207321e74c8aafb8ef48ed6c3e14e
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-08-30 21:45:08 +0800

    server.js changes

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.static('public'));
-
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 app.use('/api', apiRoutes);
