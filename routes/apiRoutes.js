57ecfc9c76bde0be8eaf6e597d95308d7a87c226
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-09-02 19:42:22 +0800

    Changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const uuid = require("uuid");
 const fs = require("fs");
+const path = require("path");
 
 router.use(express.json());
 
 router.get('/api/notes', (req, res) => {
     try {
-        const db = fs.readFileSync('./db/db.json');
+        const dbPath = path.join(__dirname, './db/db.json');
+        const db = fs.readFileSync(dbPath, 'utf-8');
         res.json(JSON.parse(db));
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while fetching notes.' });
@@ -18,23 +20,24 @@ router.post('/api/notes', (req, res) => {
     try {
         console.log('Received POST request with body:', req.body);
 
-    const dbPath = './db/db.json';
-    let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+        const dbPath = path.join(__dirname, './db/db.json'); 
+        let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
 
-    const userNote = {
-        id: uuid.v4(),
-        title: req.body.title,
-        text: req.body.text,
-    };
+        const userNote = {
+            id: uuid.v4(),
+            title: req.body.title,
+            text: req.body.text,
+        };
 
-    db.push(userNote);
-    fs.writeFileSync(dbPath, JSON.stringify(db));
+        db.push(userNote);
+        fs.writeFileSync(dbPath, JSON.stringify(db));
 
-    res.json(db); // Send the updated db object as a JSON response
+        res.json(db); 
     } catch (error) {
-        console.error(error); // Log the error for debugging
-        res.status(500).json({ error: 'An error occurred while fetching notes.' });
-    } 
-});;
+        console.error('Error in POST request:', error);
+        res.status(500).json({ error: 'An error occurred while processing the request.' });
+        response.text().then(text => console.log(text));
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
